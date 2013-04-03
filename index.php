e1919e8fb7a9b5d3e790997167018f63e82fb842
Newbilius <newbilius@gmail.com>
2013-04-03 19:33:08 +0600

    Обновление

--- index.php
+++ index.php
@@ -1,65 +1,82 @@
 <?php
-include ($_SERVER["DOCUMENT_ROOT"]. "/bitrix/modules/main/include/prolog_before.php");
+
+include ($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 include_once("orm.php");
 
-function print_pr($data){
-    echo "<pre>".print_r($data,true)."</pre>";
+function print_pr($data) {
+    echo "<pre>" . print_r($data, true) . "</pre>";
 }
 
-$news=ORM::Factory(1);
-$news->Order("ID", "desc");
-$news->Limit(2,2);
-$news->ClearLimit();
-$news_list=$news->FindAll();
-
-foreach ($news_list as $item){
-    $tmp=$item->AsArray();
-    echo $item->NAME." (".$item->ID.")";
-    echo "<br>";
+$news = ORM::Factory(4);
+$news->Order("ID", "desc")->Where("ID", "=", "96")->Find();
+print_pr($news->AsArray());
+echo "<hr>";
+$news->NAME="Орион Чокопай";
+//$news->ID="9999";
+$news->WIDTH = 25;
+//$news->COLOR = "серобуромалиновый";
+//$news->NAME="";
+if (!$news->Save()) {
+    print_pr($news->GetLastError());
+    echo "<hr>";
+    echo print_pr($news->AsArray());
 }
-echo "<hr>Всего:".$news->GetCount();
-
+echo $news->WIDTH."<BR>";
+echo $news->WIDTH__ID;
 /*
-$news=ORM::Factory(1);
-$data=$news->Where("ID","=",100)->Find();
-$data->Delete();
-print_pr($data->ASArray());
-*/
+  $news=ORM::Factory(1);
+  $news->Order("ID", "desc");
+  $news->Limit(2,2);
+  $news->ClearLimit();
+  $news_list=$news->FindAll();
+
+  foreach ($news_list as $item){
+  $tmp=$item->AsArray();
+  echo $item->NAME." (".$item->ID.")";
+  echo "<br>";
+  }
+  echo "<hr>Всего:".$news->GetCount();
+ */
+/*
+  $news=ORM::Factory(1);
+  $data=$news->Where("ID","=",100)->Find();
+  $data->Delete();
+  print_pr($data->ASArray());
+ */
 
 //$news=ORM::Factory("furniture_offers");
 //$news->Where("WIDTH", "", "180");
 //$news_list=$news->FindAll();
 
 /*
-foreach ($news_list as $item){
-    $item->NAME.=' (Тестинг\'95)';
-    $item->WIDTH+=42;
-    $tmp=$item->AsArray();
-    print_pr($tmp);
-    echo $item->COLOR;
-}*/
+  foreach ($news_list as $item){
+  $item->NAME.=' (Тестинг\'95)';
+  $item->WIDTH+=42;
+  $tmp=$item->AsArray();
+  print_pr($tmp);
+  echo $item->COLOR;
+  } */
 /*
-$news=ORM::Factory(1);
-$news->Where("ID","=",2);
-$tmp=$item->AsArray();
-foreach ($tmp as $code=>$tmp_item){
-    if (strpos($code, "~")===FALSE)
-    echo '"'.$code.'",';;
-};
-*/
+  $news=ORM::Factory(1);
+  $news->Where("ID","=",2);
+  $tmp=$item->AsArray();
+  foreach ($tmp as $code=>$tmp_item){
+  if (strpos($code, "~")===FALSE)
+  echo '"'.$code.'",';;
+  };
+ */
 //
 //$news->Find();
 //print_pr($news);
-
 //$news->Where("ID",">","0");
 /*
-$news_list=$news->FindAll();
-foreach ($news_list as $item){
-    $tmp=$item->AsArray();
-    //print_pr($tmp);
-    echo $item->NAME;
-    echo "<br>";
-    //print_pr($tmp['ID']);
-}*/
+  $news_list=$news->FindAll();
+  foreach ($news_list as $item){
+  $tmp=$item->AsArray();
+  //print_pr($tmp);
+  echo $item->NAME;
+  echo "<br>";
+  //print_pr($tmp['ID']);
+  } */
 //print_pr($news_list);
 ?>
--- orm.php
+++ orm.php
@@ -13,21 +13,6 @@ if (CModule::IncludeModule("iblock")) {
  * проверки, перехват и генерация ошибок
  */
 
-class ORMOptions {
-
-    public static $standart_fields = Array("ID", "TIMESTAMP_X", "TIMESTAMP_X_UNIX", "MODIFIED_BY",
-        "DATE_CREATE", "DATE_CREATE_UNIX", "CREATED_BY", "IBLOCK_ID", "IBLOCK_SECTION_ID",
-        "ACTIVE", "ACTIVE_FROM", "ACTIVE_TO", "DATE_ACTIVE_FROM", "DATE_ACTIVE_TO", "SORT",
-        "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT", "PREVIEW_TEXT_TYPE", "DETAIL_PICTURE",
-        "DETAIL_TEXT", "DETAIL_TEXT_TYPE", "SEARCHABLE_CONTENT", "WF_STATUS_ID",
-        "WF_PARENT_ELEMENT_ID", "WF_LAST_HISTORY_ID", "WF_NEW", "LOCK_STATUS", "WF_LOCKED_BY",
-        "WF_DATE_LOCK", "WF_COMMENTS", "IN_SECTIONS", "SHOW_COUNTER", "SHOW_COUNTER_START",
-        "CODE", "TAGS", "XML_ID", "EXTERNAL_ID", "TMP_ID", "USER_NAME", "LOCKED_USER_NAME",
-        "CREATED_USER_NAME", "LANG_DIR", "LID", "IBLOCK_TYPE_ID", "IBLOCK_CODE", "IBLOCK_NAME",
-        "IBLOCK_EXTERNAL_ID", "DETAIL_PAGE_URL", "LIST_PAGE_URL", "CREATED_DATE", "BP_PUBLISHED");
-
-}
-
 class ORM {
 
     protected $IBlockID;
@@ -40,48 +25,68 @@ class ORM {
     protected $_res = false;
     protected $_data;
     protected $_data_props;
-    protected $loaded = false;
+    protected $_loaded = false;
     protected $_changed_fields = array();
     protected $_changed_props = array();
+    protected $_tmp_el = false;
+    protected $_error_text = "";
+    protected $nochange_name = Array("ID", "IBLOCK_ID");
+    protected $standart_fields = Array("ID", "TIMESTAMP_X", "TIMESTAMP_X_UNIX", "MODIFIED_BY",
+        "DATE_CREATE", "DATE_CREATE_UNIX", "CREATED_BY", "IBLOCK_ID", "IBLOCK_SECTION_ID",
+        "ACTIVE", "ACTIVE_FROM", "ACTIVE_TO", "DATE_ACTIVE_FROM", "DATE_ACTIVE_TO", "SORT",
+        "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT", "PREVIEW_TEXT_TYPE", "DETAIL_PICTURE",
+        "DETAIL_TEXT", "DETAIL_TEXT_TYPE", "SEARCHABLE_CONTENT", "WF_STATUS_ID",
+        "WF_PARENT_ELEMENT_ID", "WF_LAST_HISTORY_ID", "WF_NEW", "LOCK_STATUS", "WF_LOCKED_BY",
+        "WF_DATE_LOCK", "WF_COMMENTS", "IN_SECTIONS", "SHOW_COUNTER", "SHOW_COUNTER_START",
+        "CODE", "TAGS", "XML_ID", "EXTERNAL_ID", "TMP_ID", "USER_NAME", "LOCKED_USER_NAME",
+        "CREATED_USER_NAME", "LANG_DIR", "LID", "IBLOCK_TYPE_ID", "IBLOCK_CODE", "IBLOCK_NAME",
+        "IBLOCK_EXTERNAL_ID", "DETAIL_PAGE_URL", "LIST_PAGE_URL", "CREATED_DATE", "BP_PUBLISHED");
 
-    public function ClearGroup(){
-        $this->arGroupBy=false;
+    public function ClearGroup() {
+        $this->arGroupBy = false;
+        return $this;
     }
-    
-    public function Group($by){
-        if (!in_array($by, $this->arGroupBy)){
-            $this->arGroupBy[]=$by;
+
+    public function Group($by) {
+        if (!in_array($by, $this->arGroupBy)) {
+            $this->arGroupBy[] = $by;
         }
+        return $this;
     }
-    
+
     public function GetCount() {
         return $this->_res->SelectedRowsCount();
     }
 
-    public function ClearLimit(){
-        $this->arNavStartParams=false;
+    public function ClearLimit() {
+        $this->arNavStartParams = false;
+        return $this;
     }
-    
-    public function Limit($count,$page=1){
-        $this->arNavStartParams=Array("nPageSize"=>$count,"iNumPage"=>$page);
+
+    public function Limit($count, $page = 1) {
+        $this->arNavStartParams = Array("nPageSize" => $count, "iNumPage" => $page);
+        return $this;
     }
-    
-    public function ClearOrder(){
-        $this->arOrder=array();
+
+    public function ClearOrder() {
+        $this->arOrder = array();
+        return $this;
     }
-    public function ClearFilter(){
-        $this->arFilter=array();
+
+    public function ClearFilter() {
+        $this->arFilter = array();
+        return $this;
     }
-    
-    protected function _PrepareOrderHow($how){
-        $true_array = Array("asc","nulls,asc","asc,nulls","desc","nulls,desc","desc,null");
-        
+
+    protected function _PrepareOrderHow($how) {
+        $true_array = Array("asc", "nulls,asc", "asc,nulls", "desc", "nulls,desc", "desc,null");
+
         if (!in_array(strtolower($how), $true_array)) {
             return "ASC";
         }
         return $how;
     }
-    
+
     protected function _PrepareWhereHow($how) {
         $true_array = Array("", "!", "><", "!><", "=", "%", "?", ">", "<", ">=", "<=");
         if (!in_array($how, $true_array)) {
@@ -92,14 +97,15 @@ class ORM {
         return $how;
     }
 
-    public function Order($what,$how){
-        $how=$this->_PrepareOrderHow($how);
-        $this->arOrder[$what]=$how;
+    public function Order($what, $how) {
+        $how = $this->_PrepareOrderHow($how);
+        $this->arOrder[$what] = $how;
+        return $this;
     }
-    
+
     public function Where($what, $how, $where) {
         $how = $this->_PrepareWhereHow($how);
-        if (!in_array($what, ORMOptions::$standart_fields)) {
+        if (!in_array($what, $this->standart_fields)) {
             if (strpos("PROPERTY_", $what) === FALSE) {
                 $what = "PROPERTY_" . $what . "_VALUE";
             };
@@ -112,7 +118,7 @@ class ORM {
         );
         return $this;
     }
-    
+
     public function AndWhere($what, $how, $where) {
         return $this->Where($what, $how, $where);
     }
@@ -144,7 +150,7 @@ class ORM {
         if ($tmp_data = $this->_res->GetNextElement()) {
             $this->_data_props = $tmp_data->GetProperties();
             $this->_data = $tmp_data->GetFields();
-            $this->loaded = true;
+            $this->_loaded = true;
         }
         return $tmp_data;
     }
@@ -158,7 +164,6 @@ class ORM {
 
     protected function _FindGo() {
         $this->_PrepareDatas();
-        //print_pr($this->arFilter);
         $this->_res = CIBlockElement::GetList(
                         $this->arOrder, $this->arFilter, $this->arGroupBy, $this->arNavStartParams, $this->arSelectFields
         );
@@ -183,11 +188,17 @@ class ORM {
         $tmp = $this->_data;
         foreach ($this->_data_props as $prop) {
             $tmp['PROPS'][$prop['CODE']] = $prop['VALUE'];
+            if (isset($prop["VALUE_ENUM_ID"])) {
+                $tmp['PROPS'][$prop['CODE'] . "__ID"] = $prop['VALUE_ENUM_ID'];
+            }
         }
         return $tmp;
     }
 
     public function __set($name, $value) {
+        if (in_array($name, $this->nochange_name)) {
+            throw new Exception("поле {$name} изменять нельзя!");
+        }
         $ok = false;
         if (isset($this->_data[$name])) {
             $this->_changed_fields[$name] = $name;
@@ -195,7 +206,18 @@ class ORM {
             $ok = true;
         };
         if (isset($this->_data_props[$name])) {
-            $this->_data_props[$name]['VALUE'] = $value;
+            if (isset($this->_data_props[$name]["VALUE_ENUM_ID"])) {
+                $this->_data_props[$name]["VALUE_ENUM_ID"] = $value;
+                $property_enum = CIBlockPropertyEnum::GetList(Array(), Array("IBLOCK_ID" => $this->IBlockID, "CODE" => $name, "ID" => $value));
+                $property_enum_value = $property_enum->GetNext();
+                if ($property_enum_value['VALUE']) {
+                    $this->_data_props[$name]['VALUE'] = $property_enum_value['VALUE'];
+                } else {
+                    die("нет такого значения свойства");
+                }
+            } else {
+                $this->_data_props[$name]['VALUE'] = $value;
+            };
             $this->_changed_props[$name] = $name;
             $ok = true;
         }
@@ -208,30 +230,70 @@ class ORM {
     public function __get($name) {
         if (isset($this->_data[$name])) {
             return $this->_data[$name];
-        };
-        if (isset($this->_data_props[$name])) {
+        } elseif (isset($this->_data_props[$name])) {
             return $this->_data_props[$name]['VALUE'];
-        }
+        } else {
+            $name2 = str_replace("__ID", "", $name);
+            if (isset($this->_data_props[$name2])) {
+                return $this->_data_props[$name2]['VALUE_ENUM_ID'];
+            }
+        };
 
         die("несуществующе поле {$name}");  //@todo эксепшн
     }
 
-    public function Save() {
-        
+    public function Delete() {
+        $ELEMENT_ID = $this->_data['ID'];
+        $this->_loaded = false;
+        $this->_changed_fields = array();
+        $this->_changed_props = array();
+        return CIBlockElement::Delete($ELEMENT_ID);
     }
 
-    public function Delete($id = 0) {
-        $ELEMENT_ID = $id;
-        if ($ELEMENT_ID == 0) {
-            $ELEMENT_ID = $this->_data['ID'];
-            $this->_data_props = array();
-            $this->_data = array();
+    public function _Update() {
+        if (count($this->_changed_fields) == 0 && count($this->_changed_props) == 0) {
+            $this->_error_text = "Ни однои поле не изменено";
+            return true;
         }
-        $this->loaded = false;
-        return CIBlockElement::Delete($ELEMENT_ID);
+        $update = array();
+        foreach ($this->_changed_fields as &$field_name) {
+            $update[$field_name] = $this->_data[$field_name];
+        };
+        if (count($this->_changed_props) > 0) {
+            foreach ($this->_data_props as $prop_name => &$prop_value) {
+                if (isset($prop_value["VALUE_ENUM_ID"])) {
+                    $update["PROPERTY_VALUES"][$prop_name] = $prop_value['VALUE_ENUM_ID'];
+                } else {
+                    $update["PROPERTY_VALUES"][$prop_name] = $prop_value['VALUE'];
+                }
+            }
+        };
+
+        if ($this->_tmp_el == false) {
+            $this->_tmp_el = new CIBlockElement;
+        }
+
+        if ($res = $this->_tmp_el->Update($this->_data['ID'], $update)) {
+            $this->_error_text = "";
+            return true;
+        } else {
+            $this->_error_text = $this->_tmp_el->LAST_ERROR;
+            return false;
+        }
+    }
+
+    public function GetLastError() {
+        return $this->_error_text;
+    }
+
+    public function Save() {
+        if ($this->_loaded) {
+            $this->_Update();
+        } else {
+            $this->_Create();
+        }
     }
 
 }
 
 ?>
-    
